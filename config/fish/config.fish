5a112a22a3775bc803e891ed67decc0f12eae28d
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-09-10 10:38:08 +0200

    Improve fish path settings
    Use $fish_user_paths instead of $PATH
    Prepend instead of append
    Only add if the path actually exists

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,10 @@
-set -x PATH $PATH \
-    $HOME/bin \
-    $HOME/.composer/vendor/bin
+# Set binary paths dependent on local environment
+[ -e "/usr/local/opt/node@10/bin" ]; and set -g fish_user_paths "/usr/local/opt/node@10/bin" $fish_user_paths
+[ -e "$HOME/.composer/vendor/bin" ]; and set -x fish_user_paths $HOME/.composer/vendor/bin $fish_user_paths
+# Keep this at the bottom to make sure $HOME/bin is the first entry
+[ -e "$HOME/bin" ]; and set -x fish_user_paths $HOME/bin $fish_user_paths
 
 source ~/.config/fish/abbrevs.fish
 
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 
-[ -e "/usr/local/opt/node@10/bin" ]; and set -g fish_user_paths "/usr/local/opt/node@10/bin" $fish_user_paths
