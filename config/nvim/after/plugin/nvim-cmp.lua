618e4172022678694ead1dd55a6c306d49724809
Gabriel Birke <gabriel.birke@posteo.net>
2021-11-04 12:02:10 +0100

    NVim: Avoid autocomplete autoselection
    Avoid autocompleting when hitting <Enter> while the autocomplete menu
    shows with no selection.

--- config/nvim/after/plugin/nvim-cmp.lua
+++ config/nvim/after/plugin/nvim-cmp.lua
@@ -12,7 +12,7 @@ cmp.setup({
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.close(),
       ['<C-y>'] = cmp.config.disable, -- If you want to remove the default `<C-y>` mapping, You can specify `cmp.config.disable` value.
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ['<CR>'] = cmp.mapping.confirm({ select = false }),
     },
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
