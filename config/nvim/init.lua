e956d9eaacf21efb57fd604c9810f3df90177ebb
Gabriel Birke <gabriel.birke@gmail.com>
2021-11-29 21:55:31 +0100

    NVim: Add LSP toggle-diagnostics command

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,6 +2,7 @@ require("options")
 require("plugins")
 require("configure_plugins")
 require("mappings")
+require("custom_functions")
 vim.cmd 'source ~/.config/nvim/commands.vim'
 
 -- Optional and machine-specific settings
---
+++ config/nvim/lua/custom_functions.lua
@@ -0,0 +1,22 @@
+-- Toggle LSP diagnostics
+-- Thanks to https://www.reddit.com/r/neovim/comments/ng0dj0/comment/gyoogr2/
+vim.g.diagnostics_active = true
+function _G.toggle_diagnostics()
+  if vim.g.diagnostics_active then
+    vim.g.diagnostics_active = false
+    vim.lsp.diagnostic.clear(0)
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
+  else
+    vim.g.diagnostics_active = true
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+      vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+      }
+    )
+  end
+end
+
+vim.api.nvim_set_keymap('n', '<leader>td', ':call v:lua.toggle_diagnostics()<CR>',  {noremap = true, silent = true})
