1ba541d8b10bdbd6ea56e05c84e9128162863007
Gabriel Birke <gabriel.birke@posteo.net>
2022-05-13 13:59:33 +0200

    NVim: Fix completion keyboard shortcuts
    The cmp plugin changed the way the default keyboard shortcuts are set
    up.
    See https://github.com/hrsh7th/nvim-cmp/issues/231#issuecomment-1098175017

--- config/nvim/after/plugin/nvim-cmp.lua
+++ config/nvim/after/plugin/nvim-cmp.lua
@@ -6,14 +6,13 @@ cmp.setup({
 			vim.fn["UltiSnips#Anon"](args.body)
 		end,
 	},
-    mapping = {
+    mapping = cmp.mapping.preset.insert({
       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.close(),
-      ['<C-y>'] = cmp.config.disable, -- If you want to remove the default `<C-y>` mapping, You can specify `cmp.config.disable` value.
       ['<CR>'] = cmp.mapping.confirm({ select = false }),
-    },
+    }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
 	  { name = 'ultisnips' },
