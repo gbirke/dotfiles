d07b854e9d4a7c90c431e60abf8105f2baca3f7c
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-02-17 16:11:30 +0100

    NVim: Automatically start language servers
    Use Mason to automatically start the installed language servers.
    Integrate previous config (for tsserver/deno) in autostart config
    
    See https://github.com/williamboman/mason-lspconfig.nvim#automatic-server-setup-advanced-feature

--- config/nvim/after/plugin/mason-lspconfig.lua
+++ config/nvim/after/plugin/mason-lspconfig.lua
@@ -1,2 +1,22 @@
+local nvim_lsp = require("lspconfig")
 require("mason").setup()
 require("mason-lspconfig").setup()
+require("mason-lspconfig").setup_handlers {
+	function (server_name)
+		nvim_lsp[server_name].setup {}
+	end,
+	-- make lsp distinguish between deno TS and node TS
+	["denols"] = function()
+		nvim_lsp.denols.setup {
+		  on_attach = on_attach,
+		  root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+		}
+	end,
+	["tsserver"] = function()
+		nvim_lsp.tsserver.setup {
+		  on_attach = on_attach,
+		  root_dir = nvim_lsp.util.root_pattern("package.json"),
+		}
+	end
+}
+
