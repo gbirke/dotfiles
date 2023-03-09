6ff619fd81603b962a820abb9de72b425a276fe0
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-03-09 20:00:59 +0100

    Add Proselint configuration
    Ignore typography rule (it requires curly quotes, which mostly gets
    added by tools, I don't want to use special characters in my Markdown)

--- install_all.sh
+++ install_all.sh
@@ -17,6 +17,7 @@ ln -s $PWD/config/dunst ~/.config/dunst
 ln -s $PWD/config/fish ~/.config/fish
 ln -s $PWD/config/nvim ~/.config/nvim
 ln -s $PWD/config/omf ~/.config/omf
+ln -s $PWD/config/proselint ~/.config/proselint
 ln -s $PWD/config/youtube-dl ~/.config/youtube-dl
 ln -s $PWD/config/sway ~/.config/sway
 ln -s $PWD/config/waybar ~/.config/waybar
