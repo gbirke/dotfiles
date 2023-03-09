80b02476a15495ade87ad330aa27ea4c022966ac
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-03-09 15:18:53 +0100

    NVim: Complete words from all open buffers
    Change `cmp` configuration to complete words from all open buffers
    
    See https://github.com/hrsh7th/cmp-buffer#get_bufnrs-type-fun-number

--- config/nvim/after/plugin/nvim-cmp.lua
+++ config/nvim/after/plugin/nvim-cmp.lua
@@ -1,11 +1,11 @@
 local cmp = require'cmp'
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			vim.fn["UltiSnips#Anon"](args.body)
-		end,
-	},
+    snippet = {
+        expand = function(args)
+            vim.fn["UltiSnips#Anon"](args.body)
+        end,
+    },
     mapping = cmp.mapping.preset.insert({
       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -15,8 +15,12 @@ cmp.setup({
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
-	  { name = 'ultisnips' },
-      { name = 'buffer' },
+      { name = 'ultisnips' },
+      { name = 'buffer', options = {
+          get_bufnrs = function()
+            return vim.api.nvim_list_bufs()
+          end
+      } },
     })
   })
 
