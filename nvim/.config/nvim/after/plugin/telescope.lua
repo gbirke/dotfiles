d8aa30a2811e8415f89ab58f3eaf1d92564ae94e
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-03-10 21:19:05 +0100

    NVim: Improve Telescope configuration
    Close on first escape
    Show hidden files by default, but exclude `.git/` directory

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,12 +1,39 @@
-require("telescope").setup {
-  extensions = {
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown {
-		-- opts could go here
-      }
-      
-    }
-  }
+local telescope = require("telescope")
+local telescopeConfig = require("telescope.config")
+local actions = require("telescope.actions")
+
+-- Clone the default Telescope configuration
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+-- I want to search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+-- I don't want to search in the `.git` directory.
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
+telescope.setup {
+	defaults = {
+		vimgrep_arguments = vimgrep_arguments,
+		mappings = {
+			i = {
+				["<esc>"] = actions.close
+			},
+		},
+	},
+	pickers = {
+		find_files = {
+			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+		},
+	},
+	extensions = {
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown {
+				-- opts could go here
+			}
+
+		}
+	}
 }
 
-require("telescope").load_extension("ui-select")
+telescope.load_extension("ui-select")
