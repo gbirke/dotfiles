41999ce5b6ea4928c6436d3b4ed9e4f4ae49557e
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-05-03 14:08:12 +0200

    Fish: Improve default paths

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,9 @@
 # Set binary paths dependent on local environment
-[ -e "/usr/local/opt/node@10/bin" ]; and set -g fish_user_paths "/usr/local/opt/node@10/bin" $fish_user_paths
-[ -e "$HOME/.composer/vendor/bin" ]; and set -g fish_user_paths $HOME/.composer/vendor/bin $fish_user_paths
+[ -e "$HOME/.composer/vendor/bin" ]; and set -u fish_user_paths $HOME/.composer/vendor/bin $fish_user_paths
 # Keep this at the bottom to make sure $HOME/bin is the first entry
-[ -e "$HOME/bin" ]; and set -g fish_user_paths $HOME/bin $fish_user_paths
+[ -e "$HOME/bin" ]; and set -u fish_user_paths $HOME/bin $fish_user_paths
+
+# TODO - when all my machines have fish 3.6, replace `set -u fish_user_paths` with `fish_add_path`
 
 source ~/.config/fish/abbrevs.fish
 
