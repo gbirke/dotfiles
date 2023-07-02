48059fd536d3868676ef54add2459d23168f017e
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-07-02 14:12:50 +0200

    NVim: Don't delete brace pairs on backspace
    Add configuration for [pear](https://github.com/steelsojka/pears.nvim)
    so I can insert braces in existing text (I often forget the surround
    plugin or find it too complicated).

--- nvim/.config/nvim/after/plugin/pears.lua
+++ nvim/.config/nvim/after/plugin/pears.lua
@@ -1,4 +1,8 @@
 -- Start auto-pair plugin
-require "pears".setup()
+require "pears".setup( function(conf)
+	-- For inserting brackets in markdown files on a laptp,
+	-- where there is no dedicated delete key, only backspace
+	conf.remove_pair_on_outer_backspace(false)
+end )
 
 
