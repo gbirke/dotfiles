7c4241549c29aa6a282fdc3b392b1012d0d78f3d
Gabriel Birke <gb@birke-software.de>
2025-01-19 15:05:49 +0100

    NVim: Add code formatting with conformer plugin
    Use configuration from [Kickstart](https://github.com/nvim-lua/kickstart.nvim/) project
    
    Add configuration for Go and PHP

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -107,8 +107,6 @@ return {
 			  -- or a suggestion from your LSP for this to activate.
 			  map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
-			  map('<leader>cf', function() vim.lsp.buf.format { async = true } end, '[C]ode [F]ormatting')
-
 			  -- WARN: This is not Goto Definition, this is Goto Declaration.
 			  --  For example, in C this would take you to the header.
 			  map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
