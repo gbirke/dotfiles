c3b5bcb36b50962cea753ebaedcd82ca04249bfc
Gabriel Birke <gb@birke-software.de>
2025-05-01 14:56:54 +0200

    NVim: Add linting with nvim-lint
    Currently you have to install the linters manually with Mason (or
    globally in your system).

---
+++ nvim/.config/nvim/lua/plugins/linting.lua
@@ -0,0 +1,43 @@
+return {
+	"mfussenegger/nvim-lint",
+	dependencies = {
+		-- We already require mason in lsp.lua, but for completeness sake and easier installation of linters we require it here again
+		"williamboman/mason.nvim",
+	},
+	event = {
+		"BufReadPre",
+		"BufNewFile",
+	},
+	config = function()
+		local lint = require("lint")
+
+		lint.linters_by_ft = {
+			python = { "flake8" },
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			dockerfile = { "hadolint" },
+			lua = { "luacheck" },
+
+			markdown = { "markdownlint", "vale" },
+			gitcommit = { "gitlint", "vale" },
+		}
+
+		local lint_augroup = vim.api.nvim_create_augroup("linting", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+
+		vim.keymap.set("n", "<leader>ll", function()
+			if vim.diagnostic.is_enabled() then
+				vim.diagnostic.disable()
+			else
+				vim.diagnostic.enable()
+				lint.try_lint()
+			end
+		end, { desc = "Toggle linting in current buffer" })
+	end,
+}
