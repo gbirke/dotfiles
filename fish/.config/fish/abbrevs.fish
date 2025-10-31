ffd5b8b1ba33fbeea206a9c50c18ea883e194b4a
Gabriel Birke <gb@birke-software.de>
2025-10-31 16:04:54 +0100

    Fish: Improve fish aliases
    Add alias for Jujutsu
    Add comments

--- fish/.config/fish/abbrevs.fish
+++ fish/.config/fish/abbrevs.fish
@@ -1,8 +1,16 @@
 if not set -q abbrs_initialized
     set -U abbrs_initialized
 
+    # Git aliases
     abbr gst 'git status'
     abbr grbc 'git rebase --continue'
     abbr gca 'git commit -a -m'
+
+    # Jujutsu aliases
+    abbr jst 'jj status'
+
+    # Directory navigation
     abbr ... 'cd ../..'
+    abbr .... 'cd ../../..'
+    abbr ..... 'cd ../../../..'
 end
